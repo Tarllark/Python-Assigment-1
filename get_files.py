965cf677b4c880a4586ebda5c9ec5fd87fe0b85a
Tarllark <33085010+Tarllark@users.noreply.github.com>
2019-02-14 12:50:18 +0100

    Added webget application

---
+++ get_files.py
@@ -0,0 +1,10 @@
+import webget
+import sys
+
+
+if __name__ == '__main__':
+
+	#data = sys.stdin.read().split('\n')
+	data = sys.argv[1:]
+	for url in data:
+		webget.download(url)
---
+++ webget.py
@@ -0,0 +1,13 @@
+import sys
+import os
+import urllib.request
+from urllib.parse import urlparse
+import shutil
+
+def download(url, to=None):
+	if to == None:
+		file_name = urlparse(url).path.split('/')[-1]
+	else:
+		file_name = to + urlparse(url).path.split('/')[-1]
+	with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+		shutil.copyfileobj(response, out_file)
